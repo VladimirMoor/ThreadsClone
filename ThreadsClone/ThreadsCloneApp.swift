926a39a813c560ff15cec434bf7b3d0cce5a4e29
VladimirMuravev <aderby3d@gmail.com>
2024-01-04 10:10:31 +0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Владимир Муравьев on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
